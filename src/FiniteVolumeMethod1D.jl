8b7d3ae178d761c46c611e1980ed9145be72ec65
DanielVandH <danj.vandenheuvel@gmail.com>
2023-06-03 11:55:48 +1000

    Returns for v1.6

--- src/FiniteVolumeMethod1D.jl
+++ src/FiniteVolumeMethod1D.jl
@@ -13,4 +13,13 @@ include("problem.jl")
 include("equations.jl")
 include("solve.jl")
 
+@static if VERSION < v"1.7"
+    struct Returns{V} <: Function
+        value::V
+        Returns{V}(value) where {V} = new{V}(value)
+        Returns(value) = new{Base._stable_typeof(value)}(value)
+    end
+    (obj::Returns)(@nospecialize(args...); @nospecialize(kw...)) = obj.value
+end
+
 end # module 
