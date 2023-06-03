f28bdb0559833a753237713a563278383e48861e
DanielVandH <danj.vandenheuvel@gmail.com>
2023-06-03 23:16:55 +1000

    Fix 1.6

--- src/FiniteVolumeMethod1D.jl
+++ src/FiniteVolumeMethod1D.jl
@@ -15,10 +15,12 @@ include("equations.jl")
 include("solve.jl")
 
 @static if VERSION < v"1.7"
+    _stable_typeof(x) = typeof(x)
+    _stable_typeof(::Type{T}) where {T} = @isdefined(T) ? Type{T} : DataType
     struct Returns{V} <: Function
         value::V
         Returns{V}(value) where {V} = new{V}(value)
-        Returns(value) = new{Base._stable_typeof(value)}(value)
+        Returns(value) = new{_stable_typeof(value)}(value)
     end
     (obj::Returns)(@nospecialize(args...); @nospecialize(kw...)) = obj.value
 end
